7d4c2cf163e99dd307dfad659b7d5a22556e6bfd
Arulmurugan-BCT <arulmurugan.s@bahwancybertek.com>
2022-12-08 12:18:17 +0530

    Added code for only uploading customer preference

---
+++ customPreferenceUpload.js
@@ -0,0 +1,184 @@
+const fs = require("fs");
+const filePath =
+  // '/hubspot-properties-export-properties-export-2022-09-15 (1).xlsx';
+  "/Indulge Client Persona 02 Nov 2022 (5).xlsx";
+var xlsx = require("node-xlsx");
+var got = require("axios");
+var obj = xlsx.parse(__dirname + filePath); // parses a file
+
+//create UserPreference post method
+const postCustomerPreference = async function (body) {
+  const url = "http://localhost:4002/api/customer-preferences/bulk";
+  return got({
+    url,
+    data: body,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIiLCJuYW1lIjoic2VsdmEiLCJlbWFpbCI6InNlbHZhbmF0aGFhbkBnbWFpbC5jb20iLCJpYXQiOjE2NjczODU4NTUsImV4cCI6MTk4Mjc0NTg1NX0.5Ti-0mdgfriUfT0NCUkSClq82P4jDc4H6FdoTFe9jVE",
+    },
+  });
+};
+
+const createQuestionnaire = async function (body) {
+  const url = "http://localhost:1337/api/questionnaires";
+  return got({
+    url,
+    data: {
+      data: body,
+    },
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer 6f04630799bfdddbaa767d4d6dda19208a1e3d9cda87698889e9136076159084ec3a28dee4fdc4de70b2e6f09e43b487d9ffb9179a9266c9f503bce09e497294e3bf10b1326b77826ec394cbf2d5d127b363b664a97a29076f91ad136417822eb30a88c590fd9c014819362759ced6c9e6129d9275c0305b058b9dc2a2b1c443",
+    },
+  });
+};
+//get questionnaires method
+const getQuestionnaire = async function (
+  params = {
+    pagination: {
+      pageSize: 100,
+    },
+  }
+) {
+  const url = "http://localhost:1337/api/questionnaires";
+  return got({
+    url,
+    params,
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer 6f04630799bfdddbaa767d4d6dda19208a1e3d9cda87698889e9136076159084ec3a28dee4fdc4de70b2e6f09e43b487d9ffb9179a9266c9f503bce09e497294e3bf10b1326b77826ec394cbf2d5d127b363b664a97a29076f91ad136417822eb30a88c590fd9c014819362759ced6c9e6129d9275c0305b058b9dc2a2b1c443",
+    },
+  });
+};
+
+//get user from 4001
+const getUser = async function (params = {}) {
+  const url = "http://localhost:4001/api/users";
+  return got({
+    url,
+    params,
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjIiLCJuYW1lIjoic2VsdmEiLCJlbWFpbCI6InNlbHZhbmF0aGFhbkBnbWFpbC5jb20iLCJpYXQiOjE2NjczODU4NTUsImV4cCI6MTk4Mjc0NTg1NX0.5Ti-0mdgfriUfT0NCUkSClq82P4jDc4H6FdoTFe9jVE",
+    },
+  });
+};
+const data = obj[0].data;
+const promises = [];
+async function createUserPreference(data) {
+  //here i am getting all cms questionnaires
+  for (let columnIndex = 0; columnIndex < 6; columnIndex++) {
+    for (let rowIndex = 9; rowIndex < data[0].length; rowIndex++) {
+      //here i am checking the user is exist or not
+      const userServiceFilter = {
+        where: {
+          or: [
+            { phone: data[columnIndex + 1][3] },
+            { email: data[columnIndex + 1][2] },
+          ],
+        },
+      };
+      const userExist = await getUser({
+        filter: JSON.stringify(userServiceFilter),
+      });
+      //   console.log("userExist?data");
+      //   console.log(userExist?.data[0]);
+      const questionnaire = await getQuestionnaire();
+      const questionData = questionnaire?.data?.data;
+        // console.log("questionData");
+        // console.log(questionData);
+      let questionNumber;
+      let questionType = "custom_preference";
+      let answer;
+      let otherAnswerd = "";
+      questionData.forEach((element) => {
+        // console.log("here comes");
+        const cmsQuestion = element?.attributes?.question;
+        const oldAnswerType = element?.attributes?.answerType;
+        // console.log(cmsQuestion);
+        // console.log(oldAnswerType);
+        if (cmsQuestion === data[0][rowIndex]) {
+          questionNumber = element?.id;
+          questionType = "default_preference";
+
+          switch (oldAnswerType) {
+            case "multi":
+              allOptions = cmsQuestion?.options;
+              each(allOptions, (option) => {
+                if (option.label === String(data[columnIndex + 1][rowIndex])) {
+                  answer = [`${String(option.id)}`];
+                }
+              });
+              break;
+            case "single":
+              answer = [`${String(cmsQuestion?.options[0]?.id)}`];
+              if (
+                cmsQuestion?.options[1]?.label ===
+                String(data[columnIndex + 1][rowIndex])
+              ) {
+                answer = [`${String(cmsQuestion?.options[1]?.id)}`];
+              }
+              break;
+            case "text":
+              answer = ["-2"];
+              otherAnswerd = String(data[columnIndex + 1][rowIndex]);
+              break;
+            case "date":
+              answer = ["-3"];
+              otherAnswerd = String(data[columnIndex + 1][rowIndex]);
+              break;
+            default:
+              break;
+          }
+        }
+      });
+      if (!questionNumber) {
+        const customerPreferenceInfo = {
+          question: data[0][rowIndex],
+          questionnaireType: questionType,
+          answerType: "text",
+          // hubspotKey: element[5],
+          isActive: true,
+          category: 2,
+        };
+        const createdQuestion = await createQuestionnaire(
+          customerPreferenceInfo
+        );
+        questionNumber = createdQuestion?.data?.data?.id;
+      }
+
+      // constructing payload for create userPreference
+      let customerPreferenceData = [];
+      if (data[columnIndex + 1][rowIndex]) {
+        // here i am checking the otherAnswer field is not empty
+        const info = {
+          question: String(questionNumber),
+          answers: answer,
+          otherAnswer: otherAnswerd, //data[1][9] data[1][10] data[1][11]
+          type: questionType,
+          userId: Number(userExist?.data[0]?.userId),
+        };
+        customerPreferenceData.push(info);
+        // console.log(customerPreferenceData);
+        // postCustomerPreference(customerPreferenceData);
+        console.log("info : ", customerPreferenceData, columnIndex);
+      }
+    }
+  }
+}
+createUserPreference(data);
+Promise.all(promises)
+  .then((resp) => {
+    console.log("success");
+  })
+  .catch((err) => {
+    console.log("failed");
+  });
